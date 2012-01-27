e90d4341e332c9a60686110faa6a11803114648f
case <case@mnemonic.(none)>
2012-01-27 22:31:36 +0100

    boids, maybe moving

--- js/game.js
+++ js/game.js
@@ -1,10 +1,68 @@
+var CONF = {
+    space_width: 10,
+    space_height: 10,
+    
+    boid_speed: 1.0,
+    boid_nbr : 100,
+}
+
+function newBoid(id, x, y, u, v) {
+    var boid = {
+	id: id,
+	pos: [ x, y, 0 ],
+	v: [ u, v, 0 ],
+	speed: CONF.boid_speed,
+    };
+    osg.Vec3.normalize(boid.v, boid.v);
+    return boid;
+};
+
+function newSpace() {
+    var id = 0;
+    var W = CONF.space_width;
+    var H = CONF.space_height;
+    var space = {
+	width: W,
+	height: H,
+	boidsList: [],
+	boidsMap: {},
+    };
+
+    space.newRandomBoid = function() {
+	var boid = newBoid(id++, Math.random()*W, Math.random()*H, Math.random(), Math.random());
+	space.boidsList.push(boid);
+	space.boidsMap[boid.id] = boid;
+	return boid;
+    };
+    
+    space.update = function(dt) {
+	
+	for(var i=0; i < space.boidsList.length; i++) {
+	    var b = space.boidsList[i];
+	    var v = osg.Vec3.mult(b.v, dt*b.speed, []);
+	    osg.Vec3.add(b.pos, v, b.pos);
+	}
+	
+    }
+
+    for(var i=0; i < CONF.boid_nbr; i++) {
+	space.newRandomBoid();
+    }
+    return space;
+}
+
 
 var MainUpdate = function() {
     this._lastUpdate = undefined;
+    this._space = newSpace();
 };
 
 MainUpdate.prototype = {
 
+    playerInput: function(event) {
+	// Player inputs go there...
+    },
+    
     update: function(node, nv) {
         var t = nv.getFrameStamp().getSimulationTime();
         if (this._lastUpdate === undefined) {
@@ -12,11 +70,7 @@ MainUpdate.prototype = {
         }
         var dt = t - this._lastUpdate;
 
-        // here you are at home
-
-        //osg.log("yeah");
-
-        // 
+	this._space.update(dt);
         
         return true;
     }
